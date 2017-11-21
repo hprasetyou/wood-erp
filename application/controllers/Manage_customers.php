dfd882553df53e32712db05c61d3047b9f917753
Heru Prasetyo Utomo <hprasetyou@hotmail.com>
2017-11-21 15:24:31 +0700

    add logger

--- application/controllers/Manage_customers.php
+++ application/controllers/Manage_customers.php
@@ -7,7 +7,11 @@ class Manage_customers extends MY_Controller{
   function __construct(){
     $this->objname = 'Partner';
     $this->tpl = 'customers';
-    $this->objobj = PartnerQuery::create()->filterByIsCustomer(true);
+    $this->objobj = PartnerQuery::create()
+    ->filterByIsCustomer(true)
+    ->filterByCompanyId(0)
+    ->_or()
+    ->filterByCompanyId(null);
     parent::__construct();
     $this->authorization->check_authorization('manage_customers');
   }
@@ -33,12 +37,6 @@ class Manage_customers extends MY_Controller{
     'IsCustomer'=>array('value'=>1)
   )){
     $data = parent::write($id,$fields);
-    if($this->input->post('Image')){
-      if(strpos($this->input->post('Image'),'base64')){
-        $this->load->helper('base64toimage');
-        $data->setImage(base64_to_img($this->input->post('Image')));
-      }
-    }
     redirect('manage_customers/detail/'.$data->getId());
   }
 
