d107c2435e6467fd31dcae716fe1d8d3df4d4952
Heru Prasetyo Utomo <hprasetyou@hotmail.com>
2017-11-24 15:56:27 +0700

    customer layout

--- application/controllers/Manage_customers.php
+++ application/controllers/Manage_customers.php
@@ -12,18 +12,8 @@ class Manage_customers extends MY_Controller{
     ->filterByCompanyId(0)
     ->_or()
     ->filterByCompanyId(null);
+
     parent::__construct();
-    $this->authorization->check_authorization('manage_customers');
-  }
-
-  function get_json(){
-    if($this->input->get('company_id')){
-      $this->objobj = PartnerQuery::create()->filterByCompanyId($this->input->get('company_id'));
-    }
-    parent::get_json();
-  }
-
-  function write($id=null){
     $this->form = array(
       'Name'=>'Name',
       'Address'=>'Address',
@@ -36,10 +26,21 @@ class Manage_customers extends MY_Controller{
       'BankDetail'=>'BankDetail',
       'IsCustomer'=>array('value'=>1)
     );
+    $this->authorization->check_authorization('manage_customers');
+  }
+
+  function get_json(){
+    if($this->input->get('company_id')){
+      $this->objobj = PartnerQuery::create()->filterByCompanyId($this->input->get('company_id'));
+    }
+    parent::get_json();
+  }
+
+  function write($id=null){
     if(!$id){
       $this->form['CompanyId']='CompanyId';
     }
-    $data = parent::write($id,$fields);
+    $data = parent::write($id);
     redirect('manage_customers/detail/'.$data->getId());
   }
 
