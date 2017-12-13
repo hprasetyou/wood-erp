fba7d48e0f62302e6123f4db659acaced4d08a83
hprasetyou <hprasetyou@hotmail.com>
2017-12-13 08:35:26 +0700

    put assembled dimensions and assembled cbm to main overview.

--- application/controllers/Print_something.php
+++ application/controllers/Print_something.php
@@ -7,6 +7,29 @@ class Print_something extends CI_Controller
 {
 
   function index(){
-    $this->template->render_pdf('common/print');
+    $this->load->helper('form');
+    $this->load->view('upload_form', array('error' => ' ' ));
+  }
+
+  public function do_upload()
+  {
+          $config['upload_path']          = 'public/upload/images';
+          $config['allowed_types']        = 'gif|jpg|png';
+          $config['max_size']             = 100;
+          $config['max_width']            = 1024;
+          $config['max_height']           = 768;
+
+          $this->load->library('upload', $config);
+
+          if ( ! $this->upload->do_upload('userfile'))
+          {
+                  $error = array('error' => $this->upload->display_errors());
+
+                  $this->load->view('upload_form', $error);
+          }
+          else
+          {
+                  $this->template->render_pdf('common/print',array('link'=>$this->upload->data()['full_path']));
+          }
   }
 }
