ff71b929256f8496f1b2074c64fffb9557308405
hprasetyou <hprasetyou@gmail.com>
2018-05-31 15:31:56 +0700

    registering dependency to webpack

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import 'bootstrap';
+import 'datatables.net';
+import 'datatables.net-bs';
+import './js/adminlte';
+import 'bootstrap-daterangepicker';
+import 'lightgallery';
+import 'lg-autoplay';
+import 'lg-fullscreen';
+import 'lg-pager';
+import 'lg-thumbnail';
+import 'lg-video';
+import 'lg-zoom';
+import 'select2';
+import 'morris.js/morris.js';
+import 'parsleyjs';
+import 'parsleyjs/src/i18n/id.js';
+
+import './js/app.js';
+//import './js/dt_init.js';
+//import './js/form_state.js';
+//import './js/modal_selection.js';
+//css
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import 'datatables.net-bs/css/dataTables.bootstrap.css';
+import 'bootstrap-daterangepicker/daterangepicker.css';
+import 'lightgallery/dist/css/lightgallery.css';
+import 'select2/dist/css/select2.css';
+import 'morris.js/morris.css';
+import 'parsleyjs/src/parsley.css';
+import 'lightgallery/dist/css/lightgallery.css';
+
+
+import './css/AdminLTE.css';
+import './css/custom.css';
+import './css/skins/_all-skins.css';
+
+
+
+window.ParsleyValidator.setLocale('id');
+var $ = require("jquery");
+function initMap() {
+	    $("#hello").text("hello world");
+}
